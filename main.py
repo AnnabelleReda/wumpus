2810a90cb722797e1adb67d2ad49290202326004
annabelle Reda <99501221+AnnabelleReda@users.noreply.github.com>
2022-02-15 17:15:26 +0000

    scared

--- main.py
+++ main.py
@@ -1,70 +1,110 @@
-alive = True
-currentRoom = 1
-caveMap = {
-  1:[3, 4, 2],
-  2:[1, 4, 5, 7],
-  3:[1, 4, 9],
-  4:[1, 2, 3, 11, 8, 7],
-  5:[2],
-  6:[7],
-  7:[6, 8, 4, 2],
-  8:[4, 7,],
-  9:[10, 12, 13, 3,],
-  10:[9],
-  11:[4, 13, 14],
-  12:[9, 13],
-  13:[12, 9, 11, 14],
-  14:[13, 11, 15],
-  15:[14, 16],
-  16:[],
+import random
+from enum import Enum
+
+class WumpusState(Enum):
+  DEAD = 0
+  AWAKE = 1
+  ASLEEP = 2
+
+gameState = {
+  "alive": True,
+  "wumpusState":WumpusState.ASLEEP,
+  "wumpusRoom": 1,
+  "currentRoom": 1,
+  "caveMap": {
+    1:[3, 4, 2],
+    2:[1, 4, 5, 7],
+    3:[1, 4, 9],
+    4:[1, 2, 3, 11, 8, 7],
+    5:[2],
+    6:[7],
+    7:[6, 8, 4, 2],
+    8:[4, 7,],
+    9:[10, 12, 13, 3,],
+    10:[9],
+    11:[4, 13, 14],
+    12:[9, 13],
+    13:[12, 9, 11, 14],
+    14:[13, 11, 15],
+    15:[14, 16],
+    16:[],
+  }
 }
 
-def niceExitList(roomExits):
-  global alive
+def numOfRooms (state):
+  count =0
+  for room in state["caveMap"]:
+    count += 1
+  return count
+
+def safeRandomRoom(state):
+  while True:
+   room = random.randint(1, numOfRooms(state))
+   roomExits = state["caveMap"][room]
+   if len(roomExits) > 0:
+     break
+  return room
+
+def newGame (state) :
+  if numOfRooms (state) < 2:
+    print ("A game that only has one room is not supported")
+    raise SystemExit
+  while state["wumpusRoom"] == state["currentRoom"]:
+    state["wumpusRoom"] = safeRandomRoom(state)
+    state["currentRoom"] = safeRandomRoom(state)
+  return
+
+def niceExitList(state):
+  currentRoom = state["currentRoom"]
+  roomExits = state["caveMap"][currentRoom]
   numExits = len(roomExits)
-  if numExits== 0:
-    alive = False
-    return "you are trapped! This room has no exits and you are starved"
+  if numExits == 0:
+    state["alive"] = False
+    return "You are trapped! This room has no exits and you have starved"
   if numExits == 1:
-    return f"this room's only exit is to room {roomExits[0]}"
+    return f"This room's only exit is to room {roomExits[0]}"
   if numExits == 2:
     return f"This room has exits to rooms {roomExits[0]} and {roomExits[1]}"
-
-  niceList = "this room has exits to rooms:" 
+  niceList = "This room has exits to rooms: "
   for exitNum in range(numExits-1):
     niceList += f"{roomExits[exitNum]}, "
   niceList += f"and {roomExits[-1]}."
-
   return niceList
 
+def look(state):
+  currentRoom = state["currentRoom"]
+  print(f"You are in room {currentRoom}")
+  if currentRoom == state["wumpusRoom"]:
+    if state["wumpusState"] == WumpusState.ASLEEP:
+      print("you see a sleeping wumpus")
+    else:
+      print("you see a wumpus looking back at you")
+  print(niceExitList(state))
 
-def look():
-  print(f"you are in room {currentRoom}")
-  print(niceExitList(caveMap[currentRoom]))
-  
-def move():
-  global currentRoom
-  nextRoom = int(input("where would you like to go?"))
-  if nextRoom not in caveMap[currentRoom]:
-    print(f"i'm sorry, you cannot get in to room {nextRoom} from here")
+def move(state):
+  currentRoom = state["currentRoom"]
+  nextRoom = int(input("where would you like to go? "))
+  if nextRoom not in state["caveMap"][currentRoom]:
+    print(f"I'm sorry, you cannot get to room {nextRoom} from here.")
     return
-    if nextRoom not in caveMap:
-      print(f"Oh no! Room {nextRoom} doesnt exist")
-      return
-  currentRoom = nextRoom
+  if nextRoom not in state["caveMap"]:
+    print(f"Oh no! Room {nextRoom} doesn't even exist!")
+    return
+  state["currentRoom"] = nextRoom
 
-print("hunt the wupus")
+newGame(gameState)
+print("Hunt the Wumpus")
 print()
-while alive:
-  look()
-  if not alive:
+while gameState["alive"]:
+  print("hint: The wumpus is in room {wumpusRoom}".format_map(gameState))
+  look(gameState)
+  if not gameState["alive"]:
     break
-  nextAction = input("\nwhats next? ")
+  nextAction = input("\nWhat's next? ")
   if nextAction.lower()[0] == "m":
-    move()
-    print("lets move!")
+    move(gameState)
     continue
   if nextAction.lower()[0] == "q":
     break
-  print (f"I'm sorry, I don't know how to do '{nextAction}'.")
-  print ("I do know how to quit!")
+  print(f"I'm sorry, I don't know how to do '{nextAction}'.")
+  print("I know how to quit.")
